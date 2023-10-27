a8c187aaff52deb288a1cc23cb086885d61d2e66
vivvk <vickykumarsunilja@gmail.com>
2023-10-27 18:03:45 +0530

    fixed footer

--- client/src/components/AppLayout.jsx
+++ client/src/components/AppLayout.jsx
@@ -8,17 +8,19 @@ export default function AppLayout() {
       <div>
         <Outlet />
       </div>
-      <footer className="hidden sm:flex justify-between bg-violet-200 p-3 bottom-0 text-end w-full  mt-10 ">
-        <Link to="/" className="mx-auto">
-          <h2 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500 ">wiki</span>
-            <span className="text-violet-400 ">Estate 🏡</span>
-          </h2>
-        </Link>
-        <span className="text-violet-700 font-bold mx-auto">
-          &copy; By Vicky Kumar {new Date().getFullYear()}
-        </span>
-      </footer>
+      <div>
+        <footer className="hidden sm:flex justify-between bg-violet-200 p-3 bottom-0 text-end w-full">
+          <Link to="/" className="mx-auto">
+            <h2 className="font-bold text-sm sm:text-xl flex flex-wrap">
+              <span className="text-slate-500 ">wiki</span>
+              <span className="text-violet-400 ">Estate 🏡</span>
+            </h2>
+          </Link>
+          <span className="text-violet-700 font-bold mx-auto">
+            &copy; By Vicky Kumar {new Date().getFullYear()}
+          </span>
+        </footer>
+      </div>
     </>
   );
 }
